af5c8a441f81627b9cff2b82c014ec6fcc52f261
seven-it-work <75720932+seven-it-work@users.noreply.github.com>
2024-12-24 14:45:26 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import './style.css'
 import App from './App.vue'
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
+import Clipboard from 'v-clipboard'
+
+
 const app = createApp(App);
 
 app.use(ElementPlus);
+app.use(Clipboard);
 
 app.mount('#app');
